5640e2934b62ace1f98d318a97bba1d947e74d41
Andriy1909 <andriy1909@yandex.ua>
2018-01-19 20:21:30 +0200

    Добавьте файлы проекта.

---
+++ WebHelloWord/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebHelloWord.Startup))]
+namespace WebHelloWord
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
